1acc68c4634a28fe453fb01b6fda89d475bc464e
Zack Stevens <zack.st7@gmail.com>
2022-07-07 11:17:54 -0600

    fix: Added outputs for eks cluster name, eks cluster arn, and eks irs… (#11)
    * fix: Added outputs for eks cluster name, eks cluster arn, and eks irsa provider arn
    
    * chore(docs): apply automatic generated docs
    
    * Remove eks name, it's not a supported field. Add eks identity oidc url as an output, fix typo in eks cluster arn output name.
    
    * chore(docs): apply automatic generated docs
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- outputs.tf
+++ outputs.tf
@@ -2,12 +2,24 @@ output "eks_cluster_id" {
   value = aws_eks_cluster.cluster.id
 }
 
+output "eks_cluster_arn" {
+  value = aws_eks_cluster.cluster.arn
+}
+
 output "eks_cluster_endpoint" {
   value = aws_eks_cluster.cluster.endpoint
 }
 
 output "eks_cluster_certificate_authority_data" {
   value = aws_eks_cluster.cluster.certificate_authority[0].data
+}
+
+output "eks_identity_oidc_url" {
+  value = aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+}
+
+output "eks_irsa_provider_arn" {
+  value = aws_iam_openid_connect_provider.irsa_provider.arn
 }
 
 output "cluster_autoscaler_irsa_role_arn" {
