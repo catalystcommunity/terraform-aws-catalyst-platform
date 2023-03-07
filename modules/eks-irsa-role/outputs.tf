9c1f6e71a58017c867f291d58783bf760056a327
Zack Stevens <zack.st7@gmail.com>
2023-03-07 12:48:25 -0700

    fix: Add role name and id to the outputs (#20)
    * fix: Add role name and id to the outputs
    
    * chore(docs): apply automatic generated docs
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- modules/eks-irsa-role/outputs.tf
+++ modules/eks-irsa-role/outputs.tf
@@ -2,3 +2,13 @@ output "role_arn" {
   description = "ARN of IAM role"
   value       = aws_iam_role.role.arn
 }
+
+output "role_name" {
+  description = "Name of the IAM role"
+  value = aws_iam_role.role.name
+}
+
+output "role_id" {
+  description = "Id of the IAM role"
+  value = aws_iam_role.role.id
+}
